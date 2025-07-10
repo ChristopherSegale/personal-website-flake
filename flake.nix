6f267016de4297aca884fd45a961010978207617
ChristopherSegale <ccs.segale@gmail.com>
2025-07-10 12:48:37 -0400

    Separating derivation definition in a separate file

--- flake.nix
+++ flake.nix
@@ -13,44 +13,23 @@
       pkgs = nixpkgs.legacyPackages.${system}.extend cl-nix-lite.overlays.default;
       name = "personal-website";
       version = "1.0";
-      web-server = pkgs.fetchFromGitHub {
-        owner = "ChristopherSegale";
-        repo = "serve-website";
-        rev = "bb64154";
-        hash = "sha256-aWkFlLxJln93GVs+qBYXdX1+/ccVUe8zxAWkBpsKJAU=";
-      };
-      contents = pkgs.fetchFromGitHub {
-        owner = "ChristopherSegale";
-        repo = "personal-website";
-        rev = "2f284cf";
-        hash = "sha256-MZ5OvLALRwYqsbZZDDkcA9S2PWvPnNvl2XFpDU9fZhU=";
-      };
-      buildInputs = with pkgs; [ openssl_3 ];
-      scriptName = "run-server";
     in {
       packages = {
-        default = pkgs.lispPackagesLite.lispDerivation {
-          pname = name;
-          inherit version buildInputs;
-          lispSystem = "serve-website";
-          lispDependencies = with pkgs.lispPackagesLite; [
-            bordeaux-threads
-            hunchentoot
-            cl-who
-          ];
-          src = web-server;
-          patches = [ ./personal-website.patch ];
-          installPhase = ''
-            mkdir -p $out/bin
-            mkdir -p $out/var/serve-website
-            echo "#!${pkgs.runtimeShell}" >> $out/bin/${scriptName}
-            printf "cd %s\n" $out/var/serve-website >> $out/bin/${scriptName}
-            echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath buildInputs} ./serve-website" >> $out/bin/${scriptName}
-            chmod 0555 $out/bin/${scriptName}
-            cp serve-website $out/var/serve-website
-            cp -r web-resources $out/var/serve-website
-            cp -r ${contents}/web-resources/* $out/var/serve-website/web-resources
-          '';
+        default = import ./package.nix {
+          inherit pkgs name version;
+          web-server = pkgs.fetchFromGitHub {
+            owner = "ChristopherSegale";
+            repo = "serve-website";
+            rev = "bb64154";
+            hash = "sha256-aWkFlLxJln93GVs+qBYXdX1+/ccVUe8zxAWkBpsKJAU=";
+          };
+          contents = pkgs.fetchFromGitHub {
+            owner = "ChristopherSegale";
+            repo = "personal-website";
+            rev = "2f284cf";
+            hash = "sha256-MZ5OvLALRwYqsbZZDDkcA9S2PWvPnNvl2XFpDU9fZhU=";
+          };
+          scriptName = "run-server";
         };
         meta.license = pkgs.lib.licenses.mit;
       };
---
+++ package.nix
@@ -0,0 +1,26 @@
+{pkgs, name, version, web-server, contents, scriptName }:
+
+pkgs.lispPackagesLite.lispDerivation {
+  pname = name;
+  inherit version web-server contents scriptName;
+  buildInputs = with pkgs; [ openssl_3 ];
+  lispSystem = "serve-website";
+  lispDependencies = with pkgs.lispPackagesLite; [
+    bordeaux-threads
+    hunchentoot
+    cl-who
+  ];
+  src = web-server;
+  patches = [ ./personal-website.patch ];
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/var/serve-website
+    echo "#!${pkgs.runtimeShell}" >> $out/bin/${scriptName}
+    printf "cd %s\n" $out/var/serve-website >> $out/bin/${scriptName}
+    echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath buildInputs} ./serve-website" >> $out/bin/${scriptName}
+    chmod 0555 $out/bin/${scriptName}
+    cp serve-website $out/var/serve-website
+    cp -r web-resources $out/var/serve-website
+    cp -r ${contents}/web-resources/* $out/var/serve-website/web-resources
+  '';
+}
